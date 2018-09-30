8422d5fd76e5ac439f18de47f7baf6e5a6c91dc5
xrfang <xrfang@gmail.com>
2018-09-30 09:31:51 +0800

    added config load skeleton

--- src/go-webserver/conf.go
+++ src/go-webserver/conf.go
@@ -4,24 +4,35 @@ import (
 	"os"
 	"path"
 	"path/filepath"
+
+	audit "github.com/xrfang/go-audit"
 )
 
 type Configuration struct {
 	LogFile string
 	Port    string
 	WebRoot string
-
+	//TODO: define configuration items
 	binPath string
 	cfgFile string
 	cfgPath string
 }
 
-func canonicalize(fn string) string {
+func (c Configuration) abs(fn string) string {
 	if fn == "" || path.IsAbs(fn) {
 		return fn
 	}
-	p, _ := filepath.Abs(path.Join(cf.binPath, fn))
+	p, _ := filepath.Abs(path.Join(c.binPath, fn))
 	return p
+}
+
+func (c *Configuration) Load(fn string) {
+	f, err := os.Open(fn)
+	audit.Assert(err)
+	defer f.Close()
+	//TODO: load configuration from f
+	c.cfgFile = c.abs(fn)
+	c.cfgPath = path.Dir(c.cfgFile)
 }
 
 var cf Configuration
@@ -31,6 +42,7 @@ func loadConfig() {
 	cf.Port = "8080"
 	cf.WebRoot = "../webroot"
 	cf.LogFile = "../log/log"
-	cf.WebRoot = canonicalize(cf.WebRoot)
-	cf.LogFile = canonicalize(cf.LogFile)
+	//TODO: load configuration from file, e.g. cf.Load(...)
+	cf.WebRoot = cf.abs(cf.WebRoot)
+	cf.LogFile = cf.abs(cf.LogFile)
 }
